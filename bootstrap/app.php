33dc1f1ee51e3896ba066611f6bba31657560f54
dawillygene <eliamariki2000@gmail.com>
2026-06-06 14:16:01 +0300

    Schedule admin dashboard metrics indexing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Console\Commands\IndexAdminDashboardMetricsCommand;
 use App\Http\Middleware\RoleMiddleware;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,6 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function (Schedule $schedule): void {
+        $schedule->command(IndexAdminDashboardMetricsCommand::class)
+            ->everyMinute()
+            ->withoutOverlapping();
+    })
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => RoleMiddleware::class,
